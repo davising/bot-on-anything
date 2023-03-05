82e3c07f7a2e1ff69ea213fcd5217c60cdfff3b3
wangjianxing <wangjianxing@qiyi.com>
2023-03-05 16:05:56 +0800

    切换到flask webserver

--- channel/httpserver/httpserver_channel.py
+++ channel/httpserver/httpserver_channel.py
@@ -1,53 +1,38 @@
-#!/usr/local/bin/python3
-
 from channel.channel import Channel
 from config import channel_conf
 from common import const
-from http.server import BaseHTTPRequestHandler, HTTPServer
-from urllib.parse import urlparse, parse_qs, unquote
-import json
+from flask import Flask, request, jsonify
+from urllib.parse import parse_qs
+from common.log import logger
+
+app = Flask(__name__)
 
 class HttpServerChannel(Channel):
     def startup(self):
-
+        app.config['JSON_AS_ASCII'] = False
         hostName = channel_conf(const.HTTPSERVER).get('host')
         serverPort = channel_conf(const.HTTPSERVER).get('port')
-        webServer = HTTPServer((hostName, serverPort), MyServer)
-        print("Server started http://%s:%s" % (hostName, serverPort))
+        app.run(hostName, serverPort, debug=True)
 
-        try:
-            webServer.serve_forever()
-        except KeyboardInterrupt:
-            pass
+@app.route('/api/openai', methods=['POST'])
+def process_submit():
+    result = ""
+    response = {
+        "data": result
+    }
 
-class MyServer(BaseHTTPRequestHandler):
-    def do_GET(self):
-        # Parse the request URL to extract the query string
-        parsed_url = urlparse(self.path)
-        query_params = parse_qs(parsed_url.query)
+    req_params_path = "query=" + request.form["query"] + "&from_user_id=" + request.form["from_user_id"]
+    req_decode_params = parse_qs(req_params_path)
+    logger.info(req_decode_params)
+    query = req_decode_params['query'][0]
+    from_user_id = req_decode_params['from_user_id'][0]
 
-        # Extract parameter from the query string
-        from_user_id = query_params.get('from_user_id', [''])[0]
-        context = {"from_user_id": from_user_id}
-        query = query_params.get('query', [''])[0]
-        query = unquote(query, encoding='utf-8')
+    if query == "" or query.isspace() or from_user_id == "" or from_user_id.isspace():
+        return jsonify(response)
 
-        result = ""
-        for res in Channel().build_reply_content(query, context):
-            result = result + "" + res
+    context = {"from_user_id": from_user_id}
+    for res in Channel().build_reply_content(query, context):
+        result = result + "" + res
 
-        # Send an HTTP response
-        self.send_response(200)
-        self.send_header('Content-type', 'application/json; charset=utf-8')
-        self.end_headers()
-
-        # Construct the response data as a Python dictionary
-        data = {
-            'data': result
-        }
-
-        # Encode the response data as a JSON string
-        message = json.dumps(data).encode('utf-8')
-        self.wfile.write(message)
-
-
+    # Encode the response data as a JSON string
+    return jsonify(response)
